88da4633e11f79a717b1da55e8c48b41852f4b1f
vvspl <vladpolt@gmail.com>
2021-07-04 15:23:21 +0300

    task

---
+++ Lesson7_plugins/Task2/src/list/todoList.js
@@ -0,0 +1,12 @@
+import { onCreateTask } from './createTask.js';
+import { onListClick } from './deleteTask.js';
+import { onToggleTask } from './updateTask.js';
+
+export const initTodoListHandlers = () => {
+  const buttonElem = document.querySelector('.btn');
+  buttonElem.addEventListener('click', onCreateTask);
+
+  const listElem = document.querySelector('.list');
+  listElem.addEventListener('click', onListClick);
+  listElem.addEventListener('click', onToggleTask);
+};
